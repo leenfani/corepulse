31d72d2047ef4df9f75de6ef158e145f4c20773b
leen fani <leenfani06@gmail.com>
2026-06-04 15:02:09 +0300

    feat: add RoleSection component with responsive layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { Box } from "@mui/material";
+import { RoleData } from "./home/RoleData";
 
 // COMPONENTS
 import HNavbar from "./home/HNavbar";
 import HHero from "./home/HHero";
 import ProblemSection from "./home/ProblemSection";
 import ScrollReveal from "./UI/ScrollReveal";
+import RoleSection from "./home/RoleSection";
 
 export default function Home() {
   return (
@@ -14,6 +16,12 @@ export default function Home() {
       <ScrollReveal>
         <ProblemSection />
       </ScrollReveal>
+
+      {RoleData.map((RoleData) => (
+        <ScrollReveal key={RoleData.title}>
+          <RoleSection {...RoleData} />
+        </ScrollReveal>
+      ))}
     </Box>
   );
 }
