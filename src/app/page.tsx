edfda36e77f4279d5277fa5f7d096e80702d831a
leen fani <leenfani06@gmail.com>
2026-06-10 15:33:41 +0300

    feat: added the footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import HHero from "./home/HHero";
 import ProblemSection from "./home/ProblemSection";
 import ScrollReveal from "./UI/ScrollReveal";
 import RoleSection from "./home/RoleSection";
+import HFooter from "./home/HFooter";
 
 export default function Home() {
   return (
@@ -22,6 +23,7 @@ export default function Home() {
           <RoleSection {...RoleData} />
         </ScrollReveal>
       ))}
+      <HFooter/>
     </Box>
   );
 }
